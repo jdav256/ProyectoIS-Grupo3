d8340d10fa3444914061bffdefaf7a17c75bfc27
hll3gt14 <37005783+hll3gt14@users.noreply.github.com>
2020-10-20 22:31:03 -0600

    migraciones

--- Proyecto/database/migrations/2014_10_12_000000_create_users_table.php
+++ Proyecto/database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,9 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('lastname');
+            $table->string('identity_number',13)->unique();
+            $table->string('gender');
+            $table->date('birthdate');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
---
+++ Proyecto/database/migrations/2020_10_21_011603_create_deductions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDeductionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('deductions', function (Blueprint $table) {
+            $table->id();
+            $table->string('description');
+            $table->string('amount');
+            $table->unsignedBigInteger('salary_id');
+            $table->foreign('salary_id')->references('id')->on('salary')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('deductions');
+    }
+}
