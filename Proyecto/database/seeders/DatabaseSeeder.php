7265b1a873a6f83a5061b8f3f8960d65a100d92d
joselp <joselp256@hotmail.es>
2020-10-26 19:58:03 -0600

    seeders

--- Proyecto/database/seeders/DatabaseSeeder.php
+++ Proyecto/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Address;
+use App\Models\AddressOrder;
 use App\Models\Employee;
 use App\Models\Order;
 use App\Models\Telephone;
@@ -18,11 +19,33 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        
         User::factory(20)->create();
         Employee::factory(10)->create();
         Address::factory(40)->create();
         Order::factory(80)->create();
         Telephone::factory(20)->create();
+        $contadorDirecciones = 1;
+        $posicion = 1;
+        $arregloPosiciones = [0,1];
+        $ordenes = Order::all();
+        foreach ($ordenes as $orden ){
+            if($contadorDirecciones>19){
+                $contadorDirecciones=1;
+            }
+            AddressOrder::create([
+                'address_id'=>$contadorDirecciones,
+                'order_id'=>$orden->id,
+                'position'=> 0
+            ]);
+            $contadorDirecciones= $contadorDirecciones+1;
+            AddressOrder::Create([
+                'address_id'=>$contadorDirecciones,
+                'order_id'=>$orden->id,
+                'position'=> 1
+            ]);  
+
+        }
         
     }
 }
